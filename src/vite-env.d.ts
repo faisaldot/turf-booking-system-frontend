c49d2d0425f00547004fd27c902dd7a7092e0c39
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2025-09-07 12:04:26 +0600

    Environment configuration setup

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	readonly VITE_API_URL: string;
+	readonly VITE_APP_URL: string;
+	readonly VITE_SSL_STORE_ID: string;
+	readonly VITE_ENABLE_DEVTOOLS: "true" | "false";
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
